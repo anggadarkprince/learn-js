247de2914bc867a185b34ed754f344bb1e6c7352
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-10-07 21:05:28 +0700

    add promise and simple custom http request

---
+++ js/async/easy-http.js
@@ -0,0 +1,45 @@
+function easyHTTP() {
+    this.http = new XMLHttpRequest();
+}
+
+// Make an HTTP GET request
+easyHTTP.prototype.get = function (url, callback) {
+    this.http.open('get', url, true);
+    var self = this;
+    this.http.onload = function () {
+        callback(self.http.status, self.http.responseText);
+    }
+    this.http.send();
+}
+
+// Make an HTTP POST request
+easyHTTP.prototype.post = function (url, data, callback) {
+    this.http.open('post', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    this.http.setRequestHeader('Access-Control-Allow-Origin', '*');
+    this.http.onload = function () {
+        callback(this.responseText);
+    }
+    this.http.send(JSON.stringify(data));
+}
+
+// Make an HTTP PUT request
+easyHTTP.prototype.put = function (url, data, callback) {
+    this.http.open('put', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    this.http.setRequestHeader('Access-Control-Allow-Origin', '*');
+    this.http.onload = function () {
+        callback(this.responseText);
+    }
+    this.http.send(JSON.stringify(data));
+}
+
+// Make an HTTP DELETE request
+easyHTTP.prototype.delete = function (url, callback) {
+    this.http.open('delete', url, true);
+    var self = this;
+    this.http.onload = function () {
+        callback(self.http.status, self.http.responseText);
+    }
+    this.http.send();
+}
---
+++ js/async/easy-request.js
@@ -0,0 +1,51 @@
+const http = new easyHTTP();
+
+// Get posts
+document.getElementById('button-get').addEventListener('click', function () {
+    document.querySelector('.loading').style.visibility = 'visible';
+    http.get('https://jsonplaceholder.typicode.com/posts', function (status, data) {
+        document.querySelector('.loading').style.visibility = 'hidden';
+        if (status === 200) {
+            document.getElementById('output').innerHTML = `<pre>${data}</pre>`;
+        } else {
+            console.log('Error : ' + status);
+        }
+    });
+});
+
+var postData = {
+    userId: 1,
+    title: 'Hello world',
+    body: 'Usto sed quo iure occaecati omnis eligendi aut ad'
+}
+
+// Post a post
+document.getElementById('button-insert').addEventListener('click', function () {
+    document.querySelector('.loading').style.visibility = 'visible';
+    http.post('https://jsonplaceholder.typicode.com/posts', postData, function (data) {
+        document.querySelector('.loading').style.visibility = 'hidden';
+        document.getElementById('output').innerHTML = `<pre>${data}</pre>`;
+    });
+});
+
+// Put a post
+document.getElementById('button-update').addEventListener('click', function () {
+    document.querySelector('.loading').style.visibility = 'visible';
+    http.put('https://jsonplaceholder.typicode.com/posts/1', postData, function (data) {
+        document.querySelector('.loading').style.visibility = 'hidden';
+        document.getElementById('output').innerHTML = `<pre>${data}</pre>`;
+    });
+});
+
+// Delete a post
+document.getElementById('button-delete').addEventListener('click', function () {
+    document.querySelector('.loading').style.visibility = 'visible';
+    http.delete('https://jsonplaceholder.typicode.com/posts/1', function (status, data) {
+        document.querySelector('.loading').style.visibility = 'hidden';
+        if (status === 200) {
+            document.getElementById('output').innerHTML = `<pre>Post 1 deleted! ${data}</pre>`;
+        } else {
+            console.log('Error : ' + status);
+        }
+    });
+});
