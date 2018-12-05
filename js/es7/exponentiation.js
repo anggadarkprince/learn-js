05154fe69aa75b9064d7f5c64f52ac73e633c1d9
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-12-05 20:47:47 +0700

    add es7 new feature

---
+++ js/es7/exponentiation.js
@@ -0,0 +1,7 @@
+let eight = Math.pow(8, 2);
+console.log(eight);
+
+console.log(8**2);
+
+eight **= 2;
+console.log(eight);
---
+++ js/es8/pad.js
@@ -0,0 +1,4 @@
+let hey = 'awesome';
+let id = 25;
+console.log(hey.padEnd(10, '!'));
+console.log(id.toString().padStart(8, '0'));
